1c8dc3eb05dbc3534d1fc9fe9d05f0de68ce97c0
Subhayan Bhattacharya <susubhat@oradev.oraclecorp.com>
2018-05-03 00:04:38 +0530

    Adding psycopg2 to the requirements.txt file

--- project/settings.py
+++ project/settings.py
@@ -2,6 +2,5 @@ import os
 
 SECRET_KEY = os.urandom(24)
 DEBUG = True
-DB_URI = "postgres://twjkqapvdyxjji:79ce19c20132fb550ba22a4becfa49830ea97eedcfde84c429ae3be4416dbded@ec2-54-204-46-236.compute-1.amazonaws.com:5432/d6kav22jlegt9r"
-SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
